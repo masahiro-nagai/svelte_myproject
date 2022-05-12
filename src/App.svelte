4196be7a2796d066961c2eb71202b9965ea439c5
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-12 23:55:49 +0900

    databinding

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script>
-	export let name;
+	let name = "Masahiro";
+	let beltColor = "black";
+
+	const handleClick = () =>{
+		beltColor = "orange";
+	};
+
+	const handleInput = (e) =>{
+		beltColor = e.target.value
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p style="color: {beltColor}">{beltColor} belt</p>
+	<button on:click={handleClick}>update belt color</button>
+	<!-- <input type="text" on:input={handleInput} value={beltColor}> -->
+	<input type="text" bind:value={beltColor}>
 </main>
 
 <style>
