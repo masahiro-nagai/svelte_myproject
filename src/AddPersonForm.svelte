178d8bb9a54313c312a5387c2537c9d080b48cc8
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-25 00:06:23 +0900

    forms2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,8 +2,12 @@
     let name
     let beltColour
     let age
+    // let fighting = false
+    // let sneaking = false
+    // let running = false
+    let skills =[]
     const handleSubmit = () =>{
-        console.log(name,beltColour,age)
+        console.log(name,beltColour,age,skills)
     }
 </script>
 
@@ -11,6 +15,19 @@
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="beltcolour" bind:value={beltColour}>
     <input type="number" placeholder="age" bind:value={age}>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>Skills:</label>
+    
+    <input type="checkbox" bind:group={skills} value="fighting">fighting<br>
+    <input type="checkbox" bind:group={skills} value="sneaking">sneaking<br>
+    <input type="checkbox" bind:group={skills} value="running">running<br>
+    <label>Belt colour</label>
+    <select bind:value={beltColour}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="brown">brown</option>
+        <option value="white">white</option>
+    </select>
     <button>Add Person</button>
 </form>
 
